6b89201f93274c15e45d198a3fca41ba594334ab
egatapova <ergatapovaer@gmail.com>
2024-08-07 23:41:25 +0400

    Added main project structure folders, added test pages, new layout

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import Layout from '../app/layouts/Layout';
+
+const About: NextPage = () => {
+  return (
+    <Layout>
+      <h1>About Us</h1>
+    </Layout>
+  );
+};
+
+export default About;
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import Layout from '../app/layouts/Layout';
+
+const Home: NextPage = () => {
+  return (
+    <Layout>
+      <h1>Welcome to our site!</h1>
+    </Layout>
+  );
+};
+
+export default Home;
